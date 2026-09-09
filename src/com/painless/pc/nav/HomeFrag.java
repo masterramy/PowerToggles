98ffa7de5fd00f61dabcd138a756a92c407a807f
masterramy <ramy.baheeg@gmail.com>
2026-09-09 09:22:37 -0700

    Modernize Home widget share backup and restore storage

--- src/com/painless/pc/nav/HomeFrag.java
+++ src/com/painless/pc/nav/HomeFrag.java
@@ -1,12 +1,16 @@
 package com.painless.pc.nav;
 
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.appwidget.AppWidgetHost;
 import android.appwidget.AppWidgetManager;
+import android.content.ClipData;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -32,6 +36,7 @@ import android.widget.RemoteViews;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.painless.pc.FileProvider;
 import com.painless.pc.PCWidgetActivity;
 import com.painless.pc.R;
 import com.painless.pc.picker.FilePicker;
@@ -46,7 +51,7 @@ public class HomeFrag extends AbsListFrag {
 
   private static final int REQUEST_BACKUP = 10;
   private static final int REQUEST_RESTORE = 11;
-  private static final String SHARE_FILE_NAME = "widget.zip";
+  private static final String DEFAULT_BACKUP_NAME = "power-toggles-widget.zip";
 
   @Thunk Context mContext;
   @Thunk LayoutInflater mLf;
@@ -54,6 +59,7 @@ public class HomeFrag extends AbsListFrag {
   private MyAdapter mAdapter;
 
   private ListItem mItemUnderAction;
+  private int mPendingWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
 
   @Override
   public void onViewCreated(View view, Bundle savedInstanceState) {
@@ -77,7 +83,6 @@ public class HomeFrag extends AbsListFrag {
       final AppWidgetManager awm = AppWidgetManager.getInstance(mContext);
       for (final int widgetId : awm.getAppWidgetIds(new ComponentName(mContext, PCWidgetActivity.class))) {
         Log.e("PC", "WidgetID " + widgetId);
-
         mAdapter.add(new ListItem(widgetId, this));
       }
     }
@@ -85,33 +90,19 @@ public class HomeFrag extends AbsListFrag {
 
   @Thunk void handleMenuClicked(View target, MenuItem item) {
     mItemUnderAction = (ListItem) target.getTag(R.id.btn_menu);
+    if (mItemUnderAction == null) {
+      return;
+    }
+    mPendingWidgetId = mItemUnderAction.widgetId;
     switch(item.getItemId()) {
       case R.id.mnu_share:
-        try {
-          BackupUtil.createBackup(mContext.openFileOutput(SHARE_FILE_NAME, Context.MODE_WORLD_READABLE), mItemUnderAction.widgetId, mContext);
-
-          Intent shareIntent = new Intent(Intent.ACTION_SEND);
-          shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(mContext.getFileStreamPath(SHARE_FILE_NAME)));
-          shareIntent.setType("application/zip");
-          startActivity(Intent.createChooser(shareIntent, getString(R.string.wp_share)));
-        } catch (Throwable e) {
-          Debug.log(e);
-          Toast.makeText(mContext, R.string.msg_unknown_error, Toast.LENGTH_LONG).show();
-        }
+        shareWidget(mPendingWidgetId);
         break;
       case R.id.mnu_backup:
-        Intent picker1 = new Intent(mContext, FilePicker.class)
-            .putExtra("savemode", true)
-            .putExtra("title", getString(R.string.wp_backup))
-            .putExtra("filter", ".zip");
-        startActivityForResult(picker1, REQUEST_BACKUP);
+        startBackupPicker();
         break;
       case R.id.mnu_restore:
-        Intent picker2 = new Intent(mContext, FilePicker.class)
-          .putExtra("savemode", false)
-          .putExtra("title", getString(R.string.wp_restore))
-          .putExtra("filter", ".zip");
-        startActivityForResult(picker2, REQUEST_RESTORE);
+        startRestorePicker();
         break;
       case R.id.mnu_delete:
         new AlertDialog.Builder(mContext)
@@ -119,7 +110,7 @@ public class HomeFrag extends AbsListFrag {
             .setMessage(Html.fromHtml(getString(R.string.wp_delete_msg)))
             .setNegativeButton(R.string.act_cancel, null)
             .setPositiveButton(R.string.act_delete, new DialogInterface.OnClickListener() {
-    
+
               @Override
               public void onClick(DialogInterface dialog, int which) {
                 dialog.dismiss();
@@ -130,7 +121,66 @@ public class HomeFrag extends AbsListFrag {
     }
   }
 
+  private void shareWidget(int widgetId) {
+    FileOutputStream out = null;
+    try {
+      File shareFile = FileProvider.widgetShareFile(mContext);
+      out = new FileOutputStream(shareFile, false);
+      BackupUtil.createBackup(out, widgetId, mContext);
+      out = null; // BackupUtil closes the supplied stream through ZipOutputStream.
+
+      Uri shareUri = Uri.parse(FileProvider.WIDGET_SHARE_URI);
+      Intent shareIntent = new Intent(Intent.ACTION_SEND)
+          .setType("application/zip")
+          .putExtra(Intent.EXTRA_STREAM, shareUri)
+          .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+          .setClipData(ClipData.newRawUri("Power Toggles widget backup", shareUri));
+      startActivity(Intent.createChooser(shareIntent, getString(R.string.wp_share)));
+    } catch (Throwable e) {
+      Debug.log(e);
+      Toast.makeText(mContext, R.string.msg_unknown_error, Toast.LENGTH_LONG).show();
+    } finally {
+      if (out != null) {
+        try { out.close(); } catch (Exception e) { Debug.log(e); }
+      }
+    }
+  }
+
+  private void startBackupPicker() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+      Intent picker = new Intent(Intent.ACTION_CREATE_DOCUMENT)
+          .addCategory(Intent.CATEGORY_OPENABLE)
+          .setType("application/zip")
+          .putExtra(Intent.EXTRA_TITLE, DEFAULT_BACKUP_NAME);
+      startActivityForResult(picker, REQUEST_BACKUP);
+    } else {
+      Intent picker = new Intent(mContext, FilePicker.class)
+          .putExtra("savemode", true)
+          .putExtra("title", getString(R.string.wp_backup))
+          .putExtra("filter", ".zip");
+      startActivityForResult(picker, REQUEST_BACKUP);
+    }
+  }
+
+  private void startRestorePicker() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+      Intent picker = new Intent(Intent.ACTION_OPEN_DOCUMENT)
+          .addCategory(Intent.CATEGORY_OPENABLE)
+          .setType("application/zip");
+      startActivityForResult(picker, REQUEST_RESTORE);
+    } else {
+      Intent picker = new Intent(mContext, FilePicker.class)
+          .putExtra("savemode", false)
+          .putExtra("title", getString(R.string.wp_restore))
+          .putExtra("filter", ".zip");
+      startActivityForResult(picker, REQUEST_RESTORE);
+    }
+  }
+
   @Thunk void doDelete() {
+    if (mItemUnderAction == null) {
+      return;
+    }
     try {
       AppWidgetHost host = new AppWidgetHost(mContext.getApplicationContext(), 0);
       host.deleteAppWidgetId(mItemUnderAction.widgetId);
@@ -143,29 +193,108 @@ public class HomeFrag extends AbsListFrag {
 
   @Override
   public void onActivityResult(int requestCode, int resultCode, Intent data) {
-    if (resultCode != Activity.RESULT_OK) {
-       return;
+    if (resultCode != Activity.RESULT_OK || data == null
+        || mPendingWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
+      return;
     }
-    String fileName = data.getStringExtra("file");
+
     if (requestCode == REQUEST_BACKUP) {
-      String[] msgArray = getResources().getStringArray(R.array.wc_export_msg);
-      try {
-        BackupUtil.createBackup(new FileOutputStream(fileName), mItemUnderAction.widgetId, mContext);
-        String msg = String.format(msgArray[1], fileName);
-        Toast.makeText(mContext, msg, Toast.LENGTH_LONG).show();
-      } catch (Throwable e) {
-        Debug.log(e);
-        Toast.makeText(mContext, msgArray[2], Toast.LENGTH_LONG).show();
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+        backupToUri(data.getData(), mPendingWidgetId);
+      } else {
+        backupToLegacyPath(data.getStringExtra("file"), mPendingWidgetId);
       }
     } else if (requestCode == REQUEST_RESTORE) {
-      if (!BackupUtil.importBackup(fileName, mContext, mItemUnderAction.widgetId)) {
+      boolean restored = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT
+          ? restoreFromUri(data.getData(), mPendingWidgetId)
+          : restoreFromLegacyPath(data.getStringExtra("file"), mPendingWidgetId);
+      if (!restored) {
         Toast.makeText(mContext, getResources().getStringArray(R.array.wc_import_msg)[1], Toast.LENGTH_LONG).show();
       }
       onResume();
     }
   }
 
-  
+  private void backupToUri(Uri destination, int widgetId) {
+    String[] msgArray = getResources().getStringArray(R.array.wc_export_msg);
+    if (destination == null) {
+      Toast.makeText(mContext, msgArray[2], Toast.LENGTH_LONG).show();
+      return;
+    }
+    OutputStream out = null;
+    try {
+      out = mContext.getContentResolver().openOutputStream(destination, "w");
+      if (out == null) {
+        throw new IllegalStateException("Unable to open backup destination");
+      }
+      BackupUtil.createBackup(out, widgetId, mContext);
+      out = null; // BackupUtil closes via ZipOutputStream.
+      Toast.makeText(mContext, String.format(msgArray[1], destination.toString()), Toast.LENGTH_LONG).show();
+    } catch (Throwable e) {
+      Debug.log(e);
+      Toast.makeText(mContext, msgArray[2], Toast.LENGTH_LONG).show();
+    } finally {
+      if (out != null) {
+        try { out.close(); } catch (Exception e) { Debug.log(e); }
+      }
+    }
+  }
+
+  private void backupToLegacyPath(String fileName, int widgetId) {
+    String[] msgArray = getResources().getStringArray(R.array.wc_export_msg);
+    if (fileName == null) {
+      Toast.makeText(mContext, msgArray[2], Toast.LENGTH_LONG).show();
+      return;
+    }
+    try {
+      BackupUtil.createBackup(new FileOutputStream(fileName), widgetId, mContext);
+      Toast.makeText(mContext, String.format(msgArray[1], fileName), Toast.LENGTH_LONG).show();
+    } catch (Throwable e) {
+      Debug.log(e);
+      Toast.makeText(mContext, msgArray[2], Toast.LENGTH_LONG).show();
+    }
+  }
+
+  private boolean restoreFromUri(Uri source, int widgetId) {
+    if (source == null) {
+      return false;
+    }
+    File temp = null;
+    InputStream in = null;
+    FileOutputStream out = null;
+    try {
+      temp = File.createTempFile("power_toggles_widget_restore_", ".zip", mContext.getCacheDir());
+      in = mContext.getContentResolver().openInputStream(source);
+      if (in == null) {
+        return false;
+      }
+      out = new FileOutputStream(temp);
+      BackupUtil.copy(in, out);
+      out.close();
+      out = null;
+      in.close();
+      in = null;
+      return BackupUtil.importBackup(temp.getAbsolutePath(), mContext, widgetId);
+    } catch (Throwable e) {
+      Debug.log(e);
+      return false;
+    } finally {
+      if (out != null) {
+        try { out.close(); } catch (Exception e) { Debug.log(e); }
+      }
+      if (in != null) {
+        try { in.close(); } catch (Exception e) { Debug.log(e); }
+      }
+      if (temp != null && temp.exists() && !temp.delete()) {
+        Debug.log(new Exception("Unable to delete temporary widget restore file"));
+      }
+    }
+  }
+
+  private boolean restoreFromLegacyPath(String fileName, int widgetId) {
+    return fileName != null && BackupUtil.importBackup(fileName, mContext, widgetId);
+  }
+
   @Override
   public void onListItemClick(ListView l, View v, int position, long id) {
     Globals.showWidgetConfig(mAdapter.getItem(position).widgetId, mContext, false);
