c7537047d0b78c616aeeaef4e5e7439cc737c9d3
masterramy <ramy.baheeg@gmail.com>
2026-09-09 23:21:23 -0700

    widget: classify both notification rows correctly

--- src/com/painless/pc/util/WidgetSetting.java
+++ src/com/painless/pc/util/WidgetSetting.java
@@ -2,6 +2,7 @@
 package com.painless.pc.util;
 
 import static com.painless.pc.singleton.Globals.STATUS_BAR_WIDGET_ID;
+import static com.painless.pc.singleton.Globals.STATUS_BAR_WIDGET_ID_2;
 import static com.painless.pc.util.SettingsDecoder.CLICK_TYPE_DEFAULT;
 import static com.painless.pc.util.SettingsDecoder.CLICK_TYPE_KITKAT;
 import static com.painless.pc.util.SettingsDecoder.CLICK_TYPE_RIPPLE;
@@ -111,7 +112,8 @@ public final class WidgetSetting {
 			}
 		}
 		this.hideDividers = decoder.is(KEY_HIDE_DIVIDERS, true);
-		this.rvFactory = RVFactory.get(context, decoder, (widgetId == STATUS_BAR_WIDGET_ID) || (widgetId == STATUS_BAR_WIDGET_ID));
+		this.rvFactory = RVFactory.get(context, decoder,
+        (widgetId == STATUS_BAR_WIDGET_ID) || (widgetId == STATUS_BAR_WIDGET_ID_2));
 
 		this.batteryEnabled = batteryEnabled;
 		this.isLockScreenWidget = isLockWidget(context, widgetId);
